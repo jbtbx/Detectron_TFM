df37529032da010357890c92e84ca313610e9c4b
jbtbx <bazanfanolajohan@gmail.com>
2022-09-26 15:39:31 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -9,17 +9,6 @@ import random
 import cv2
 import matplotlib.pyplot as plt
 
-def plot_samples(dataset_name, n=1):
-    dataset_custom = DatasetCatalog.get(dataset_name)
-    dataset_custom_metadata = MetadataCatalog.get(dataset_name)
-
-    for s in random.sample(dataset_custom, n):
-        img = cv2.imread(s["file_name"])
-        v = Visualizer(img[:,:,::-1], metadata=dataset_custom_metadata, scale=0.5)
-        v = v.draw_dataset_dict(s)
-        plt.figure(figsize=(15,20))
-        plt.imshow(v.get_image())
-        plt.show()
 
 def get_train_cfg(config_file_path, checkpoint_url, train_dataset_name, test_dataset_name, num_classes, device, output_dir):
     cfg = get_cfg()
